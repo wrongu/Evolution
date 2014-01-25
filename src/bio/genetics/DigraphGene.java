cca7f36fc741da897aa312ea760138fb24d6ed9f
wrongu <lange.richard.d@gmail.com>
2014-01-25 11:48:12 -0500

    DigraphGene has Cycle and Terminal edges
    'jump'/'goto' edges have been removed
    traversal algorithm was cleaned up
    serialize/deserialize updated accordingly
    javadoc comments added

--- src/bio/genetics/DigraphGene.java
+++ src/bio/genetics/DigraphGene.java
@@ -24,13 +24,15 @@ import static applet.Util.clamp_radians;
  * A gene for creating organisms by traversing a directed graph. Nodes in the graph correspond to joints in structure,
  * and edges correspond to limbs. Edges may be recursively followed to make substructures.
  * 
- * TODO javadoc
  * TODO tests (serialization, mutation, f(g) properties)
  */
 public class DigraphGene implements IGene<Organism> {
 	
-	// members of the digraphgene
+	/**
+	 * graph traversal always starts from the same root node (the first node in serialization)
+	 */
 	private GraphNode root;
+	
 	// note that if we could guarantee that this graph is connected, then only the root would be needed
 	// to track the full structure (by traversing it). But here I assume that there may be 'latent' nodes
 	// or edges with the gene that will be mutated with everything else. Having a list of nodes and edges
@@ -39,6 +41,7 @@ public class DigraphGene implements IGene<Organism> {
 	private ArrayList<GraphEdge> edges;
 	
 	// shared reference vector for 'horizontal'
+	// used to 
 	private static final Vector2d HORIZONTAL = new Vector2d(1.0, 0.0);
 	
 	// lists used during construction
@@ -52,18 +55,27 @@ public class DigraphGene implements IGene<Organism> {
 		edges = new ArrayList<GraphEdge>();
 	}
 	
+	/**
+	 * This class acts as a node in the directed graph and is responsible for instantiating PointMasses during traversal
+	 * @author wrongu
+	 */
 	private static class GraphNode{
-		// usual graph-y things
+		/** edges in which this node is 'from' */
 		private ArrayList<GraphEdge> edges_out;
+		/** just for logistics, each node gets a unique id. this is unchanged by mutation. */
 		private int local_uid;
-		// special values for graph traversal algorithm
+		/** traversal is made easier by keeping track of the last _instances_ that this node created */
 		public PointMass last_instance;
 		public Rod last_incident_rod;
-		// genetic stuff that defines a joint
+		/** mass of the resultant pointmass(es) */
 		public double mass;
+		/** whether or not this pointmass is a joint (i.e. it has limits on rotation) */
 		public boolean is_joint;
+		/** low and high limits - only useful if it is a joint */
 		public double rest_low, rest_high;
+		/** if it is a joint, it may be either passive or driven (non-muscle or muscle) */
 		public boolean is_muscle;
+		/** strength of the muscle - only used if 'is_muscle' is true */
 		public double muscle_strength;
 		
 		/**
@@ -117,6 +129,11 @@ public class DigraphGene implements IGene<Organism> {
 			return this.local_uid;
 		}
 		
+		/**
+		 * used for instantiating a pointmass from this node's properties
+		 * @param incident the rod of which the new mass will be the 2nd point (null only when instantiating the root)
+		 * @return the new PointMass instance
+		 */
 		public PointMass createPointMass(Rod incident){
 			this.last_instance = new PointMass(this.mass);
 			this.last_incident_rod = incident;
@@ -124,24 +141,35 @@ public class DigraphGene implements IGene<Organism> {
 		}
 	}
 	
+	/**
+	 * This class acts as an edge in the directed graph and is responsible for instantiating Rods during traversal
+	 * @author wrongu
+	 */
 	private static class GraphEdge{
-		// graph-y things
+		/** in terms of the directed graph, the 'source' and 'destination' nodes */
 		private GraphNode from, to;
+		/** maximum number of times to follow this edge in a given branch of DFS traversal */
 		public int max_recurse;
+		/** in the current traversal, the number of times this edge has been traversed */
 		public int current_recurse;
-		// special values for graph traversal algorithm
-		private boolean is_structure;
+		/** whether or not this edge should create a closed loop (to the last created instance of 'this.to') */
+		public boolean is_cyclic;
+		/** terminal edges are only followed when a node's sibling edges' recursion is maxed out */
+		public boolean is_terminal;
 		// TODO scale (or other affine transform) for recursive steps
-		// genetic/organism stuff for making rods
+		/** rods have a minimum and maximum rest length */
 		public double rest_low, rest_high;
+		/** rods may or may not be driven */
 		public boolean is_muscle;
+		/** if rods of this edge are driven, this is their strength multiplier */
 		public double muscle_strength;
 		
-		public GraphEdge(GraphNode from, GraphNode to, int rec, double rl, double rh, boolean muscle, double strength){
+		public GraphEdge(GraphNode from, GraphNode to, int rec, boolean cyc, boolean term, double rl, double rh, boolean muscle, double strength){
 			this.from = from;
 			this.to = to;
 			this.max_recurse = rec;
-			this.is_structure = true;
+			this.is_cyclic = cyc;
+			this.is_terminal = term;
 			this.rest_low = rl;
 			this.rest_high = rh;
 			this.is_muscle = muscle;
@@ -150,21 +178,13 @@ public class DigraphGene implements IGene<Organism> {
 			this.from.addEdge(this);
 		}
 		
-		public GraphEdge(GraphNode from, GraphNode to, int rec){
-			this(from, to, rec, 0.0, 0.0, false, 0.0);
-			this.is_structure = false;
-		}
-		
 		public void recursionReset(){
 			this.current_recurse = 0;
-		}
-		
-		public boolean isStructure(){
-			return this.is_structure;
 		}
 	}
 	
 	public DigraphGene mutate(double rate) {
+		// TODO
 		// (maybe) alter existing graph elements
 		for(GraphNode n: this.nodes.values()){
 			
@@ -178,7 +198,15 @@ public class DigraphGene implements IGene<Organism> {
 		
 		return null;
 	}
-
+	
+	/**
+	 * Express this gene as an organism using graph traversal
+	 * @see traverse_helper
+	 * 
+	 * @param posx the x position of the root (first-created point)
+	 * @param posy the y position of the root (first-created point)
+	 * @param e the environment into which this organism should be added
+	 */
 	public Organism create(double posx, double posy, Environment e) {
 		// ensure that all edges are reset in terms of recursion depth
 		for(GraphEdge ed : this.edges) ed.recursionReset();
@@ -200,39 +228,29 @@ public class DigraphGene implements IGene<Organism> {
 		return o;
 	}
 	
+	/**
+	 * Create a rod and the destination's pointmass. add in all joints and muscles as specified.
+	 * @param e
+	 * @param parent
+	 */
 	private void instantiateGraphEdge(GraphEdge e, GraphNode parent){
-		// "goto" only works if destination already exists.
-		// (that is, "goto" edges will instantiate objects iff the destination node is yet unvisited)
-		if(e.isStructure() || e.to.last_instance == null){
-			// instantiate
+		// create a rod for this edge with it's first point equal to
+		// where we are coming from
+		Rod r = new Rod(e.rest_low, e.rest_high);
+		this.all_rods.add(r);
+		r.addPoint(parent.last_instance);
 
-			// create a rod for this edge with it's first point equal to
-			// where we are coming from
-			Rod r = new Rod(e.rest_low, e.rest_high);
-			this.all_rods.add(r);
-			r.addPoint(parent.last_instance);
-			
-			// if 'parent' is a joint, set it's parameters here
-			if(parent.is_joint && parent.last_incident_rod != null){
-				Joint j = new Joint(
-						clamp_radians(parent.rest_low), clamp_radians(parent.rest_high),
-						parent.last_instance, parent.last_incident_rod, r);
-				this.all_joints.add(j);
-				// if parent is a driven joint, add the muscle
-				Muscle m = new Muscle(j, parent.muscle_strength);
-				this.all_muscles.add(m);
-			}
-			
-			// create muscle for this rod if specified
-			if(e.is_muscle){
-				Muscle m = new Muscle(r, e.muscle_strength);
-				this.all_muscles.add(m);
-			}
-			
-			// create point-mass at destination node
-			PointMass next = e.to.createPointMass(r);
+		PointMass next;
+		// note that "cycle" only works if destination already exists - otherwise it acts
+		// as non-cyclic and creates the destination
+		if(e.is_cyclic && e.to.last_instance != null){
+			next = e.to.last_instance;
+			e.to.last_incident_rod = r;
+		} else {
+			// instantiate destination pointmass
+			next = e.to.createPointMass(r);
 			this.all_points.add(next);
-			// decide where it goes using some trig
+			// decide where in space it goes using some trig
 			double l = (r.getRestValue1() + r.getRestValue2()) / 2.0;
 			// angle of the rod based on joint and previous rod
 			double angle = parent.last_incident_rod.asVector().angle(HORIZONTAL);
@@ -245,11 +263,35 @@ public class DigraphGene implements IGene<Organism> {
 			// set the position
 			next.initPosition(x, y);
 		}
+		// add this pointmass as the other end of the rod
+		r.addPoint(next);
+			
+		// if 'parent' is a joint, set it's parameters here, now that both affected rods are created
+		if(parent.is_joint && parent.last_incident_rod != null){
+			Joint j = new Joint(
+					clamp_radians(parent.rest_low), clamp_radians(parent.rest_high),
+					parent.last_instance, parent.last_incident_rod, r);
+			this.all_joints.add(j);
+			// if parent is a driven joint, add the muscle
+			if(parent.is_muscle){
+				Muscle m = new Muscle(j, parent.muscle_strength);
+				this.all_muscles.add(m);
+			}
+		}
+		
+		// create muscle for this rod if specified
+		if(e.is_muscle){
+			Muscle m = new Muscle(r, e.muscle_strength);
+			this.all_muscles.add(m);
+		}
 	}
 	
 	private void traverse_helper(GraphNode root){
+		boolean recursion_taken = false;
+		// handle non-terminal edges
 		for(GraphEdge e : root.edges_out){
-			if(e.current_recurse <= e.max_recurse){
+			if(!e.is_terminal && e.current_recurse <= e.max_recurse){
+				recursion_taken = true;
 				// mark another step of dfs traversal
 				e.current_recurse += 1;
 				// create structures
@@ -262,6 +304,23 @@ public class DigraphGene implements IGene<Organism> {
 				e.recursionReset();
 			}
 		}
+		if(!recursion_taken){
+			// handle terminal edges
+			for(GraphEdge e : root.edges_out){
+				if(e.is_terminal && e.current_recurse <= e.max_recurse){
+					// mark another step of dfs traversal
+					e.current_recurse += 1;
+					// create structures
+					this.instantiateGraphEdge(e, root);
+					// recurse (DFS)
+					// note that if no structure was created, this is basically where "goto" happens
+					this.traverse_helper(e.to);
+					// when traversal is done (i.e. when the above line is finished),
+					// set recursive depth back to 0
+					e.recursionReset();
+				}
+			}
+		}
 	}
 
 	public void serialize(OutputStream s) throws IOException {
@@ -269,7 +328,7 @@ public class DigraphGene implements IGene<Organism> {
 		// NOTE that local_uid is never mutated but only used for convenience when
 		// serializing and deserializing.
 		//
-		// write a bunch of vertices
+		// write all nodes
 		for(GraphNode node : this.nodes.values()){
 			// write node id first
 			dest.writeInt(node.getId());
@@ -286,21 +345,20 @@ public class DigraphGene implements IGene<Organism> {
 		}
 		// separator to denote end of nodes (avoiding -1 since that marks end-of-file)
 		dest.writeInt(-2);
-		// write a bunch of edges
+		// write all edges
 		for(GraphEdge edge : this.edges){
 			// write graph porperties
 			dest.writeInt(edge.from.getId());
 			dest.writeInt(edge.to.getId());
 			dest.writeInt(edge.max_recurse);
-			dest.writeBoolean(edge.isStructure());
-			if(edge.isStructure()){
-				// write rod structure properties
-				dest.writeDouble(edge.rest_low);
-				dest.writeDouble(edge.rest_high);
-				// write muscle properties
-				dest.writeBoolean(edge.is_muscle);
-				if(edge.is_muscle) dest.writeDouble(edge.muscle_strength);
-			}
+			dest.writeBoolean(edge.is_cyclic);
+			dest.writeBoolean(edge.is_terminal);
+			// write rod structure properties
+			dest.writeDouble(edge.rest_low);
+			dest.writeDouble(edge.rest_high);
+			// write muscle properties
+			dest.writeBoolean(edge.is_muscle);
+			if(edge.is_muscle) dest.writeDouble(edge.muscle_strength);
 		}
 	}
 
@@ -343,22 +401,17 @@ public class DigraphGene implements IGene<Organism> {
 				int to_id = source.readInt();
 				// read recursive depth
 				int rec = source.readInt();
-				boolean structural = source.readBoolean();
-				if(!structural){
-					// create edge
-					e = new GraphEdge(this.nodes.get(from_id), this.nodes.get(to_id), rec);
-					this.edges.add(e);
-				} else{
-					// read structure properties
-					double rl = source.readDouble();
-					double rh = source.readDouble();
-					// read muscle properties
-					boolean muscle = source.readBoolean();
-					double strength = muscle ? source.readDouble() : 0.0;
-					// create edge
-					e = new GraphEdge(this.nodes.get(from_id), this.nodes.get(to_id), rec, rl, rh, muscle, strength);
-					this.edges.add(e);
-				}
+				boolean cyc = source.readBoolean();
+				boolean term = source.readBoolean();
+				// read structure properties
+				double rl = source.readDouble();
+				double rh = source.readDouble();
+				// read muscle properties
+				boolean muscle = source.readBoolean();
+				double strength = muscle ? source.readDouble() : 0.0;
+				// create edge
+				e = new GraphEdge(this.nodes.get(from_id), this.nodes.get(to_id), rec, cyc, term, rl, rh, muscle, strength);
+				this.edges.add(e);
 			}
 		}
 	}
