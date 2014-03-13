3a2f310cf60b0b45bef2bdd4caaba21754c29bb9
wrongu <lange.richard.d@gmail.com>
2014-03-13 17:15:54 -0400

    moved Neuron and NeuronConnection to inside ANN (clean up package)

--- src/main/java/bio/ann/ANN.java
+++ src/main/java/bio/ann/ANN.java
@@ -1,7 +1,9 @@
 package bio.ann;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ANN {
 	
@@ -37,5 +39,111 @@ public class ANN {
 			nc.activate();
 		for(Neuron n : neurons)
 			n.flush();
+	}
+	
+	public static class Neuron {
+		
+		private static final float DEFAULT_ACTION_POTENTIAL = 10F;
+		private static final float DEPOLARIZATION = -3F;
+		private static final float DECAY = 0.95F;
+
+		public static final int TYPE_INPUT  = 0;
+		public static final int TYPE_HIDDEN = 1;	
+		public static final int TYPE_OUTPUT = 2;
+		
+		private float threshold;
+		private float action_potential;
+		private float activation;
+		private float next_activation;
+		private int type;
+		private ArrayList<NeuronConnection> conn_in, conn_out;
+		
+		public Neuron(float thresh, int t, float ap){
+			threshold = thresh;
+			activation = 0f;
+			action_potential = ap;
+			type = t;
+			conn_in  = new ArrayList<NeuronConnection>();
+			conn_out = new ArrayList<NeuronConnection>();
+		}
+		
+		public Neuron(float thresh){
+			this(thresh, TYPE_HIDDEN, DEFAULT_ACTION_POTENTIAL);
+		}
+		
+		public Neuron(float thresh, float ap){
+			this(thresh, TYPE_HIDDEN, ap);
+		}
+		
+		public void connect(NeuronConnection pair){
+			if(pair.getFrom() == this){
+				conn_out.add(pair);
+			} else if(pair.getTo() == this){
+				conn_in.add(pair);
+			}
+		}
+		
+		public void addActivation(float amount){
+			next_activation += amount;
+		}
+		
+		public void flush(){
+			if(next_activation > threshold){
+				activation = action_potential;
+				next_activation = DEPOLARIZATION;
+			} else{
+				activation = next_activation * DECAY;
+				next_activation = 0F;
+			}
+		}
+		
+		public float getActivation(){
+			return activation;
+		}
+		
+		public int getType(){
+			return type;
+		}
+		
+		public void setType(int t){
+			type = t;
+		}
+	}
+	
+	public static class NeuronConnection {
+		
+		public static Map<Neuron, Neuron> NEURON_MAP;
+		public static final int TYPE_IN  = 0;
+		public static final int TYPE_OUT = 1;
+		
+		private float weight;
+		private Neuron from, to;
+		
+		public NeuronConnection(float w, Neuron f, Neuron t){
+			weight = w;
+			from = f;
+			to = t;
+			
+			from.connect(this);
+			to.connect(this);
+
+			NEURON_MAP.put(from, to);
+		}
+		
+		public void activate(){
+			to.addActivation(from.getActivation() * weight);
+		}
+		
+		public Neuron getFrom(){
+			return from;
+		}
+		
+		public Neuron getTo(){
+			return to;
+		}
+		
+		static{
+			NEURON_MAP = new HashMap<Neuron, Neuron>();
+		}
 	}
 }
