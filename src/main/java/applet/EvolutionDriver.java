026ad04ca16e1e6e0906f12169b125b145cc5415
wrongu <lange.richard.d@gmail.com>
2014-04-16 15:23:44 -0400

    fixed weird fullscreen window bug

--- src/main/java/applet/EvolutionDriver.java
+++ src/main/java/applet/EvolutionDriver.java
@@ -43,7 +43,8 @@ public class EvolutionDriver implements Runnable {
 		fps = MAX_FPS;
 		frame_counter = 0;
 		paused = true; sp_down = false; mouse_hold = false; first_frame = true;
-
+		canvas = c;
+		
 		initEnvironment();
 	}
 
