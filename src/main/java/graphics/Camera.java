17cb1a90ca6ff71307cb5c46f9c845ce92289a8f
ewy-man <emmett.wyman@gmail.com>
2014-06-06 15:10:50 -0400

    smooth scroll and zoom

--- src/main/java/graphics/Camera.java
+++ src/main/java/graphics/Camera.java
@@ -7,12 +7,18 @@ public class Camera {
 	public static final double ZOOM_MIN = 0.01;
 	public static final double ZOOM_MAX = 100.0;
 	
+	public static final double SCROLL_EASE = 4.0;
+	public static final double ZOOM_EASE = 3.0;
+	
 	public double x, y;
 	public double zoom;
+	private double ease_x, ease_y, ease_zoom;
 	
 	public Camera(){
 		x = y = 0.0;
 		zoom = 1.0;
+		ease_x = ease_y = 0.0;
+		ease_zoom = 1.0;
 	}
 	
 	public void shift(double dx, double dy){
@@ -27,8 +33,11 @@ public class Camera {
 	}
 	
 	public void glSetView(){
-		glTranslated(x, y, 0.0);
-		glScaled(zoom, zoom, 1.0);
+		ease_x += (x - ease_x) / SCROLL_EASE;
+		ease_y += (y - ease_y) / SCROLL_EASE;
+		ease_zoom += (zoom - ease_zoom) / ZOOM_EASE;
+		glTranslated(ease_x, ease_y, 0.0);
+		glScaled(ease_zoom, ease_zoom, 1.0);
 	}
 
 }
