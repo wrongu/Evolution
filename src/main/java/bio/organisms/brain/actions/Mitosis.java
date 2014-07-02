9ae19805a81d152ab0d4ebf340efcfed33a2fb26
ewy-man <emmett.wyman@gmail.com>
2014-07-02 11:58:00 -0400

    git?

---
+++ src/main/java/bio/organisms/brain/actions/Mitosis.java
@@ -0,0 +1,42 @@
+package bio.organisms.brain.actions;
+
+import environment.Environment;
+import applet.Config;
+import bio.organisms.AbstractOrganism;
+import bio.organisms.SimpleCircleOrganism;
+import bio.organisms.brain.ActionSystem;
+
+public class Mitosis extends ActionSystem{
+	
+	private double MITOSIS_THRESHOLD = Config.instance.getDouble("ACT_MITOSIS_THRESHOLD");
+
+	public Mitosis(Environment e, int id) {
+		super(e, id, 0, 0);
+	}
+
+	@Override
+	public void performAll(double dt) {
+		for(AbstractOrganism org : env.getAll()) {
+			if(org instanceof SimpleCircleOrganism) {
+				SimpleCircleOrganism sco = (SimpleCircleOrganism)org;
+				
+				if(sco.getBrainOutput(output_id) > MITOSIS_THRESHOLD ) {
+					/*
+					 * Okay here is where we have options.
+					 * 1. Have the parent give birth to offsping - age is not reset.
+					 * 2. Have parent birth offspring and reset parent's age.
+					 * 3. Delete parent and produce two new offspring.
+					 * Right now, we are set at 1, because it's easiest. We will
+					 * re-address this when we have a better implementation for
+					 * age.
+					 */
+					SimpleCircleOrganism offspring = (SimpleCircleOrganism) sco.beget(env, null);
+					
+					if(offspring.is_alive())
+						env.addOrganism(offspring);
+				}
+			}
+		}
+	}
+
+}
